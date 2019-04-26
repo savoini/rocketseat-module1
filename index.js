1b3f461e71ac7ca2226440b9e97e0d59351dfa6a
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-04-25 22:46:14 -0300

    Aula 7

--- index.js
+++ index.js
@@ -14,8 +14,17 @@ nunjunks.configure('views', {
 app.set('view engine', 'njk');
 
 
+const users = ['Gilberto R.', 'Diego Fernandes', 'Robson Marques'];
+
+
 app.get('/', (req, res) => {
-    return res.render('list', {name: "Robson"});
+    return res.render('list', {users});
 });
+
+
+app.get('/new', (req, res) => {
+    return res.render('new');
+})
+
 
 app.listen(3000);
