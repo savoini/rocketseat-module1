b2ac8fa490ae63a0a8e41bad52ef5366c73edb38
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-04-25 22:39:42 -0300

    Aula 6

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
 const express = require('express');
+const nunjunks = require('nunjucks');
 
 const app = express();
 
 
-const logMiddleware = (req, res, next) => {
-    console.log(
-        `HOST: ${req.headers.host} | URL ${req.url} | METHOD: ${req.method}`
-    );
-
-    req.appName = "GoNode";
-
-    return next();
-};
+nunjunks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true,
+});
 
 
-app.use(logMiddleware);
+app.set('view engine', 'njk');
 
 
 app.get('/', (req, res) => {
-    return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
+    return res.render('list', {name: "Robson"});
 });
-
-
-app.get('/login', (req, res) => {
-    return res.send("Login");
-});
-
-
-app.get('/nome/:name', (req, res) => {
-    return res.send(`Welcome, ${req.params.name}`);
-});
-
-
-app.get('/json/:name', (req, res) => {
-    return res.json({
-        message: `Welcome, ${req.params.name}`
-    });
-});
-
 
 app.listen(3000);
