14f23bbbd16bdcfc97f506b7271bf21b8cc95b87
Gilberto Savoini <gilberto.savoini@gmail.com>
2019-04-25 23:25:01 -0300

    Aula 11

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-const express = require('express');
-const nunjunks = require('nunjucks');
+const express = require("express");
+const nunjunks = require("nunjucks");
 
 const app = express();
 
-
-nunjunks.configure('views', {
-    autoescape: true,
-    express: app,
-    watch: true,
+nunjunks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "njk");
 
-app.use(express.urlencoded({ extended: false}));
-app.set('view engine', 'njk');
+const users = ["Gilberto R.", "Diego Fernandes", "Robson Marques"];
 
-
-const users = ['Gilberto R.', 'Diego Fernandes', 'Robson Marques'];
-
-
-app.get('/', (req, res) => {
-    return res.render('list', {users});
+app.get("/", (req, res) => {
+  return res.render("list", { users });
 });
 
-
-app.get('/new', (req, res) => {
-    return res.render('new');
+app.get("/new", (req, res) => {
+  return res.render("new");
 });
 
-
-app.post('/create', (req, res) => {
-    console.log(req.body);
-    users.push(req.body.user);
-    return res.redirect("/");
-})
-
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  users.push(req.body.user);
+  return res.redirect("/");
+});
 
 app.listen(3000);
